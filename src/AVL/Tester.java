f746196d9be5f9d6a68a6e7efcf28bb3bec27909
almogby-void <benyochanan@mail.tau.ac.il>
2021-05-19 21:53:59 +0300

    loop in an external method.

--- src/AVL/Tester.java
+++ src/AVL/Tester.java
@@ -8,31 +8,15 @@ public class Tester {
 
 	public static void main(String[] args) {
 		AVLTree t = new AVLTree();
-		System.out.println(t.getRoot().getHeight());
-		t.insert(5, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(4, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(3, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(2, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(1, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(8, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(7, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(6, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(11, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(12, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(9, true);
-		System.out.println(t.getRoot().getHeight());
-		t.insert(10, true);
-		System.out.println(t.getRoot().getHeight());
+		int[] l = {5, 4, 3, 2, 1, 8, 7, 6, 11, 12, 9, 10};
+		insertHeight(t, l);
+	}
+	
+	public static void insertHeight(AVLTree t, int[] l) {
+		for (int i : l) {
+			t.insert(i, true);
+			System.out.println(t.getRoot().getHeight());
+		}
 	}
 	
 	public static void print(AVLNode root) {
